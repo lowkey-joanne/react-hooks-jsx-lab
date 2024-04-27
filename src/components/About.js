fffa6fd2e50fff9771c8456d1e64a6eb4eed66ee
joanne <joannembula33@gmail.com>
2024-04-27 14:10:56 +0300

    complete

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,16 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return(
+    <div id="about">
+      <h2>About Me</h2>
+      <p>
+        Learning React-hooks-jsx-lab
+      </p>
+      <img src={image} alt="I made this" />
+    </div>
+  );
 }
+
 
 export default About;
